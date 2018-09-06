0a75e8105323dac40c88dd4f38b8eb4f795d689a
Calvin Brownlee <calvinbrownlee@gmail.com>
2018-09-06 18:03:20 -0400

    feat: Build list of tags from Counter.state.count

--- src/componenets/counter.jsx
+++ src/componenets/counter.jsx
@@ -2,20 +2,23 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 0
   };
+
+  handleIncrement() {
+    console.log("Increment Clicked");
+  }
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
@@ -25,11 +28,7 @@ class Counter extends Component {
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
-  }
+  formatCount() {}
 }
 
 export default Counter;
